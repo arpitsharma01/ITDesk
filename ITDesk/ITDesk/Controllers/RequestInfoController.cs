b678a2d9dfd82ea42d84cdcb56cb07e49477b5ee
Arpit Sharma <82054906+arpitsharma01@users.noreply.github.com>
2021-05-12 18:01:06 +0530

    Add files via upload

---
+++ ITDesk/ITDesk/Controllers/RequestInfoController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ITDesk.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace ITDesk.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestInfoController : ControllerBase
+    {
+        ITDeskContext _context;
+        private IConfiguration _config;
+
+        public RequestInfoController(IConfiguration config, ITDeskContext context)
+        {
+            _config = config;
+            _context = context;
+        }
+        // GET: api/RequestInfo
+        [HttpGet]
+        [Route("[action]")]
+        // GET: api/requestInfo/requestStatus?status=false
+        public ActionResult requestStatus(bool status)
+        {
+            if (status == false)
+            {
+                var query = (from R in _context.RequestInfo
+                             where R.Status == false
+                             select new
+                             {
+                                 R.Id,
+                                 R.EmployeeEmail,
+                                 R.DeviceName,
+                             }).ToList();
+                return Ok(query);
+            }
+            else
+            {
+                var query = (from R in _context.RequestInfo
+                             where R.Status == true
+                             select new
+                             {
+                                 R.Id,
+                                 R.EmployeeEmail,
+                                 R.DeviceName,
+                             }).ToList();
+                return Ok(query);
+            }
+        }
+        // POST: api/RequestInfo/request
+        [HttpPost]
+        [Route("[action]")]
+        public ActionResult request([FromBody] RequestInfo newrequest)
+        {
+            RequestInfo requestInfo = new RequestInfo();
+            requestInfo.EmployeeEmail = newrequest.EmployeeEmail;
+            requestInfo.DeviceName = newrequest.DeviceName;
+            _context.RequestInfo.Add(requestInfo);
+            _context.SaveChanges();
+            return Ok(requestInfo);
+        }
+
+        // PUT: api/RequestInfo/updateStatus/5
+        [HttpPut("{id}")]
+        [Route("[action]/{id}")]
+        public ActionResult updateStatus(int id, [FromBody] RequestInfo request)
+        {
+            RequestInfo requestInfo = _context.RequestInfo.FirstOrDefault(d => d.Id == id);
+            requestInfo.Status = request.Status;
+            _context.RequestInfo.Update(requestInfo);
+            _context.SaveChanges();
+            return Ok(requestInfo);
+        }
+    }
+}
---
+++ ITDesk/ITDesk/Models/RequestInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ITDesk.Models
+{
+    public partial class RequestInfo
+    {
+        public int Id { get; set; }
+        public string EmployeeEmail { get; set; }
+        public string DeviceName { get; set; }
+        public bool Status { get; set; }
+    }
+}
